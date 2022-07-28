a8fd2520e3b59fc1a42f8581a939ec73e6c5f1e8
Samuel Alexandre Barbosa <samuelbhalexandre@hotmail.com>
2022-07-28 12:20:25 -0300

    Segundo exemplo
    Atribuição via desestruturação com mais de uma lista.

---
+++ aula32/index1.js
@@ -0,0 +1,8 @@
+// Atribuição via desestruturação via Arrys.
+// Neste exemplo usamos listas dentro de uma lista.
+//                   0          1          2
+//                0  1  2    0  1  2    0  1  2
+const numeros = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+
+const [lista1, lista2, lista3] = numeros;
+console.log(lista3[1]);
