b327962f6b14850a64e9743541ee53adb703086e
Samuel Alexandre Barbosa <samuelbhalexandre@hotmail.com>
2022-08-01 10:08:08 -0300

    Estrutura de repetição 'for'
    Estrutura de repetição simples com 'for' e exemplos com arrays.

--- aula34/index.js
+++ aula34/index.js
@@ -1,1 +1,11 @@
-// Estrutura de repetição "for"
+// Estrutura de repetição "for" clássico, simples, verboso.
+// for (i = 0; i <= 5; i++) {
+//     console.log(`Linha ${i}`);
+// }
+
+// Estrutura de repetição e arrays.
+const frutas = ['Uva', 'Maçã', 'Pêra', 'Goiaba'];
+
+for (i = 0; i < frutas.length; i++) {
+    console.log(`Índice ${i}:`, frutas[i]);
+}
