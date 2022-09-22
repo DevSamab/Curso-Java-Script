7fd4ef8922d8e32d6342ce31a96e7dd15fe027e5
Samuel Alexandre Barbosa <95145316+DevSamab@users.noreply.github.com>
2022-09-22 11:50:59 -0300

    Exercício de repetição
    Estrutura de repetição.

--- aula11/js/script.js
+++ aula11/js/script.js
@@ -8,3 +8,15 @@ n1 = parseFloat(n1); //Outra forma seria n1 = Number(n1)
 n2 = parseFloat(n2); //Outra forma seria n2 = Number(n2)
 const resultado = n1 + n2;
 alert(`A soma dos dois números é ${resultado}`);
+
+//Estudando estrutura de repetição feito como exercício da aula de JavaScript.
+let contador = 1;
+while(contador <= 10){
+    if (contador == 1)
+        console.log(`A estrutura se repetiu ${contador} vez`);
+    else
+        console.log(`A estrutura se repetiu ${contador} vezes.`);
+    contador++; //contador = contador + 1;
+}
+
+console.log("A estrutura de repetição foi finalizada.");
