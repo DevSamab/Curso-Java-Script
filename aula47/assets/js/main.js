38aa66fb0d5d80362c4d1f0e50d4f2b8ecaa186f
Samuel Alexandre Barbosa <samuelbhalexandre@hotmail.com>
2022-12-19 15:07:07 -0300

    Meu timer

--- aula47/assets/js/main.js
+++ aula47/assets/js/main.js
@@ -12,18 +12,29 @@ const pausar = document.querySelector('.pausar');
 const zerar = document.querySelector('.zerar');
 
 let segundos = 0;
+let timer;
 
 function iniciaRelogio() {
-    const timer = setInterval(function() {
+    timer = setInterval(function() {
         segundos++;
         relogio.innerHTML = criaHoraDosSegundos(segundos);
     }, 1000);
 }
 
 iniciar.addEventListener('click', function(event) {
+    relogio.classList.remove('pausado');
+    clearInterval(timer);
     iniciaRelogio();
 });
 
+pausar.addEventListener('click', function(event) {
+    clearInterval(timer);
+    relogio.classList.add('pausado');
+});
+
 zerar.addEventListener('click', function(event) {
-    alert('Cliquei em zerar.');
+    relogio.classList.remove('pausado');
+    clearInterval(timer);
+    relogio.innerHTML = '00:00:00';
+    segundos = 0;
 });
---
+++ aula47/assets/js/rascunho.js
@@ -0,0 +1,21 @@
+// document.addEventListener('click', function(event) {
+//     const element = event.target;
+
+//     if(element.classList.contains('zerar')) {
+//         relogio.classList.remove('pausado');
+//         clearInterval(timer);
+//         relogio.innerHTML = '00:00:00';
+//         segundos = 0;
+//     }
+
+//     if(element.classList.contains('iniciar')) {
+//         relogio.classList.remove('pausado');
+//         clearInterval(timer);
+//         iniciaRelogio();
+//     }
+
+//     if(element.classList.contains('pausar')) {
+//         clearInterval(timer);
+//         relogio.classList.add('pausado');
+//     }
+// });
