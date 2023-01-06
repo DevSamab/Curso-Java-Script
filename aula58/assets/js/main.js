c0929e1030dffdce848edc8e50b55c99cc8906ce
Samuel Alexandre Barbosa <samuelbhalexandre@hotmail.com>
2023-01-05 22:03:06 -0300

    calculadora

--- aula58/assets/js/main.js
+++ aula58/assets/js/main.js
@@ -1,9 +1,53 @@
 function Calculadora() {
-    document.querySelector('.display');
+    this.display = document.querySelector('.display');
 
-    function iniciaCalculadora() {
+    this.inicia = () => {
+        this.capturaCliques();
+        this.capturaEnter();
+    };
 
+    this.capturaEnter = () => {
+        document.addEventListener('keyup', e => {
+            if (e.keyCode !== 13) return;
+            this.realizaConta();
+        });
+    };
+
+    this.capturaCliques = () => {
+        document.addEventListener('click', event => {
+            const el = event.target;
+            if (el.classList.contains('btn-num')) this.addNumDisplay(el);
+            if (el.classList.contains('btn-clear')) this.clear();
+            if (el.classList.contains('btn-del')) this.del();
+            if (el.classList.contains('btn-eq')) this.realizaConta();
+        });
+    };
+
+    this.addNumDisplay = el => {
+        this.display.value += el.innerText;
+        this.display.focus();
     }
+
+    this.clear = () => this.display.value = '';
+    this.del = () => this.display.value = this.display.value.slice(0, -1);
+
+    this.realizaConta = () => {
+        try {
+            const conta = eval(this.display.value);
+
+            if (!conta) {
+                alert('Conta inválida.');
+                return;
+            }
+
+            this.display.value = conta;
+
+        } catch {
+            alert('Conta inválida.');
+            return;
+        }
+    };
 }
 
 const calculadora = new Calculadora();
+calculadora.inicia();
