8aa2dbc71e385a5464550cf7423fe24926d9f04a
Samuel Alexandre Barbosa <samuelbhalexandre@hotmail.com>
2023-01-07 18:39:10 -0300

    aula61d

---
+++ aula61/aula61d.js
@@ -0,0 +1,26 @@
+function* geradora2() {
+    let i = 0;
+
+    while(true) {
+        yield i;
+        i++;
+    }
+}
+
+function* geradora3() {
+    yield 0;
+    yield 1;
+    yield 2;
+}
+
+function* geradora4() {
+    yield* geradora3();
+    yield 3;
+    yield 4;
+    yield 5;
+}
+
+const g4 = geradora4();
+for (let valor of g4) {
+    console.log(valor);
+}
