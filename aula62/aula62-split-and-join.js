a01faef1ee63b362c64caddc0a995315f839bf7b
Samuel Alexandre Barbosa <samuelbhalexandre@hotmail.com>
2023-01-09 15:01:25 -0300

    arrays
    split() e join()

---
+++ aula62/aula62-split-and-join.js
@@ -0,0 +1,13 @@
+// o método spli() converte uma string em um array
+
+const nome = 'Samuel Alexandre Barbosa';
+const nomes = nome.split(' ');
+
+console.log(nomes);
+
+// para transformar um array em string, usa-se join()
+
+const nomes = ['Samuel', 'Alexandre', 'Barbosa'];
+const nome = nomes.join(' ');
+
+console.log(nome);
