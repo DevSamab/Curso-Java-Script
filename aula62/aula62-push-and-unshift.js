5a246980a34f1ce89850ebe5898fdea036c6e836
Samuel Alexandre Barbosa <samuelbhalexandre@hotmail.com>
2023-01-09 15:00:41 -0300

    arrays
    push() e unshift()

---
+++ aula62/aula62-push-and-unshift.js
@@ -0,0 +1,6 @@
+const nomes = ['Eduardo', 'Maria', 'Joana'];
+nomes.push('João'); // método push() adiciona um elemento no final do array
+
+// unshift adiciona elementos no começo do array, mas altera todos os índices dos elementos
+
+console.log(nomes);
