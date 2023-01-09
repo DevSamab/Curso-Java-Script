729d7809909f3d616c9962804e31f1acab981f10
Samuel Alexandre Barbosa <samuelbhalexandre@hotmail.com>
2023-01-09 15:00:19 -0300

    arrays
    pop() e shift()

---
+++ aula62/aula62-pop-and-shift.js
@@ -0,0 +1,6 @@
+const nomes = ['Eduardo', 'Maria', 'Joana'];
+const removido = nomes.pop(); // Essa linha usa o método pop para remover o último elemento do índice e atribui esse elemento à constante removido
+
+// pop() remove do final e shift() remove do começo
+
+console.log(nomes, removido);
