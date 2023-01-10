eb86878b7ac01b5c507361a16401fe2f6c334749
Samuel Alexandre Barbosa <samuelbhalexandre@hotmail.com>
2023-01-10 17:12:52 -0300

    filter()

---
+++ aula63/aula63.js
@@ -0,0 +1,6 @@
+// Para filtrar um array usa-se o método filter()
+
+const number = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 20, 22, 27];
+const filteredNumber = number.filter(value => value > 10);
+
+console.log(filteredNumber)
