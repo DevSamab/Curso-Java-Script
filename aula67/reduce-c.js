a189c86647e0cd0d12b2b77f18338fd601771791
Samuel Alexandre Barbosa <samuelbhalexandre@hotmail.com>
2023-01-12 14:15:13 -0300

    Create reduce-c.js

---
+++ aula67/reduce-c.js
@@ -0,0 +1,16 @@
+// retorne a pessoa mais velha
+
+const pessoas = [
+    {nome: 'Luiz', idade: 62},
+    {nome: 'Maria', idade: 23},
+    {nome: 'Eduardo', idade: 55},
+    {nome: 'Letícia', idade: 10},
+    {nome: 'Rosana', idade: 64},
+    {nome: 'Wallace', idade: 63}
+];
+
+const maisVelha = pessoas.reduce(function(acumulador, valor) {
+    if(acumulador.idade > valor.idade) return acumulador;
+    return valor;
+});
+console.log(maisVelha);
