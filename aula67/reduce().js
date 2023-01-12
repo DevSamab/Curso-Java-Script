43c864d0db3750e72fefcc2c548b4e1ce1c940ba
Samuel Alexandre Barbosa <samuelbhalexandre@hotmail.com>
2023-01-12 14:14:53 -0300

    Update reduce().js

--- aula67/reduce().js
+++ aula67/reduce().js
@@ -1,6 +1,10 @@
 // .reduce(acumulador, valor, indice, array)
 
+// somando todos os números (reduce)
+
 const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27];
-const total = numeros.reduce(function() {
-    console.log();
-}, 0);
+const total = numeros.reduce(function(acumulador, valor, indice, array) {
+    acumulador += valor;
+    return acumulador;
+}, 0); // se eu não mandar esse valor, o acumulador assume o primeiro valor do array
+console.log(total);
