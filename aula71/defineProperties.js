601da4eb5f5b3dd43205731a3e30ff27256768d3
Samuel Alexandre Barbosa <samuelbhalexandre@hotmail.com>
2023-01-16 21:04:22 -0300

    defineProperties

--- aula71/defineProperties.js
+++ aula71/defineProperties.js
@@ -3,8 +3,24 @@ function Produto(nome, preco, estoque) {
         enumerable: true,
         value: estoque,
         writable: false,
+        configurable: true
     });
+
+    Object.defineProperties(this, {
+        nome: {
+            enumerable: true,
+            value: nome,
+            writable: true,
+            configurable: true
+        },
+        preco: {
+            enumerable: true,
+            value: preco,
+            writable: true,
+            configurable: true
+        },
+    })
 }
 
 const p1 = new Produto('Camiseta', 20, 3);
-console.log(p1);
+console.log(Object.keys(p1));
