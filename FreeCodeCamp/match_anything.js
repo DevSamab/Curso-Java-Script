c6995946c13ff609e34a5a146c02ef40aa4de699
Samuel Alexandre Barbosa <samuelbhalexandre@hotmail.com>
2023-05-15 17:56:43 -0300

    Create match_anything.js

---
+++ FreeCodeCamp/match_anything.js
@@ -0,0 +1,5 @@
+//The wildcard character . will match any one character. The wildcard is also called dot and period. You can use the wildcard character just like any other character in the regex. For example, if you wanted to match hug, huh, hut, and hum, you can use the regex /hu./ to match all four words.
+
+let exampleStr = "Let's have fun with regular expressions!";
+let unRegex = /.un/;
+let result = unRegex.test(exampleStr);
